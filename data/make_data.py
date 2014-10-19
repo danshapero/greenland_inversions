75fc8c1a818dd82d2da948ff158e28432562feb0
danshapero <shapero.daniel@gmail.com>
2014-10-19 12:45:48 -0700

    Changed the data fetch script to get the new surface & bed DEMs for Jak

--- data/make_data.py
+++ data/make_data.py
@@ -3,7 +3,12 @@
 import os
 import zipfile
 
-# Dictionary to store names of glacier data files and URLs for where to find them on the internet
+# -----------------------
+# Retrieve velocity data
+# -----------------------
+
+# Dictionary to store names of glacier velocity data files and URLs for where
+# to find them on the internet
 helheim_files = {
     'TSX_E66.50N_27Jan09_07Feb09': 'ftp://sidads.colorado.edu/pub/DATASETS/nsidc0481_MEASURES_greenland_V01/Ecoast-66.50N/TSX_Jan-27-2009_Feb-07-2009_19-57-11/'}
 
@@ -34,7 +39,24 @@ for glacier in glacier_data.keys():
     for filestem in glacier_data[glacier].keys():
         for e in extensions:
             filename = filestem + e
-            if not os.path.exists(glacier+'/'+filename):
+            if not os.path.exists(glacier + '/' + filename):
                 url = glacier_data[glacier][filestem] + filename
-                os.system('wget '+url+' -P '+glacier)
+                os.system('wget ' + url + ' -P ' + glacier)
+
+
+
+# -----------------------------
+# Retrieve DEMs for Jakobshavn
+# -----------------------------
+
+url = "http://students.washington.edu/shapero/jakobshavn/"
+if not os.path.exists("jakobshavn/dem13Mar.smooth"):
+    os.system("wget " + url + "dem13Mar.smooth -P jakobshavn")
+    os.system("wget " + url + "dem13Mar.smooth.geodat -P jakobshavn")
+
+if not os.path.exists("jakobshavn/Jakobshavn_2006_2009_Composite.zip"):
+    os.system("wget https://data.cresis.ku.edu/data/grids/"
+                    "Jakobshavn_2006_2009_Composite.zip -P jakobshavn")
+
+
 
