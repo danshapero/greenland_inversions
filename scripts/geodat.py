62cc6428d7428afede064f383d821f8c4213be16
danshapero <shapero.daniel@gmail.com>
2015-02-08 13:35:12 -0800

    Docstring and readability improvements in geodat reader

--- scripts/geodat.py
+++ scripts/geodat.py
@@ -6,12 +6,26 @@ import math
 
 
 def read_geodat(filename):
+    """
+    Read in one of Ian's geodat files.
+
+    Parameters:
+    ==========
+    filename: the name of the geodat file to read; expects that there is
+              also a file called "filename.geodat", which contains info
+              on grid size, spacing, etc.
+
+    Returns:
+    =======
+    x, y: the grid coordinates of the output field
+    data: output field
+    """
 
     def _read_geodat(filename):
         # read in the .geodat file, which stores the number of pixels,
         # the pixel size and the location of the lower left corner
         geodatfile = open(filename, "r")
-        xgeo = np.zeros((3,2))
+        xgeo = np.zeros((3, 2))
         i = 0
         while True:
             line = geodatfile.readline().split()
@@ -25,17 +39,19 @@ def read_geodat(filename):
             if len(line) == 0:
                 break
         geodatfile.close()
-        xgeo[2,:] = xgeo[2,:] * 1000.0
+        xgeo[2, :] = xgeo[2, :] * 1000.0
         return xgeo
 
     # Get the data about the grid from the .geodat file
     xgeo = _read_geodat(filename + ".geodat")
     nx, ny = map(int, xgeo[0,:])
-    dx, dy = xgeo[1,:]
-    xo, yo = xgeo[2,:]
+    dx, dy = xgeo[1, :]
+    xo, yo = xgeo[2, :]
+
     x = np.zeros(nx)
     for i in range(nx):
         x[i] = xo + i * dx
+
     y = np.zeros(ny)
     for i in range(ny):
         y[i] = yo + i * dy
