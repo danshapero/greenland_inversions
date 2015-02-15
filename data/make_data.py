95e4dfec0b8a44178c8006f051750e25a8135714
danshapero <shapero.daniel@gmail.com>
2015-02-15 11:17:59 -0800

    Data fetching script now gets Morlighem data set from NSIDC and new velocity mosaics from my personal website

--- data/make_data.py
+++ data/make_data.py
@@ -26,8 +26,9 @@ def main(argv):                                                                #
     # Retrieve elevation data
     # ------------------------
     if dem_source == "morlighem":
-        os.system("wget ftp://sidads.colorado.edu/DATASETS/"
-                  "IDBMG4_BedMachineGr/MCdataset-2014-11-19.nc")
+        if not os.path.exists("MCdataset-2014-11-19.nc"):
+            os.system("wget ftp://sidads.colorado.edu/DATASETS/"
+                      "IDBMG4_BedMachineGr/MCdataset-2014-11-19.nc")
     else:
         os.system("wget ftp://data.cresis.ku.edu/data/temp/"
                   "for_OIBlandice/Helheim_2006_2013_Composite.zip "
@@ -43,40 +44,23 @@ def main(argv):                                                                #
     # -----------------------
     # Retrieve velocity data
     # -----------------------
-
-    # Dictionary to store names of glacier velocity data files and URLs for
-    # where to find them on the internet
-    helheim_files = {
-        'TSX_E66.50N_27Jan09_07Feb09': 'ftp://sidads.colorado.edu/pub/DATASETS/nsidc0481_MEASURES_greenland_V01/Ecoast-66.50N/TSX_Jan-27-2009_Feb-07-2009_19-57-11/'}
-
-    jakobshavn_files = {
-        'mosaicOffsets': 'http://students.washington.edu/shapero/jakobshavn/'
-    }
-
-    kangerd_files = {
-        'TSX_E68.80N_28Jan09_08Feb09': 'ftp://sidads.colorado.edu/pub/DATASETS/nsidc0481_MEASURES_greenland_V01/Ecoast-68.80N/TSX_Jan-28-2009_Feb-08-2009_19-40-29/'}
-
-    glacier_data = {
-        'helheim': helheim_files,
-        'jakobshavn': jakobshavn_files,
-        'kangerd': kangerd_files
-    }
+    glaciers = ["helheim", "kangerd", "jakobshavn"]
 
     # Make directories
-    for glacier in glacier_data.keys():
+    for glacier in glaciers:
         if not os.path.exists(glacier):
             os.makedirs(glacier)
 
     # Retrieve velocity data
     extensions = ['.vx', '.vx.geodat', '.vy', '.vy.geodat',
                     '.ex', '.ex.geodat', '.ey', '.ey.geodat', '.meta']
-    for glacier in glacier_data.keys():
-        for filestem in glacier_data[glacier].keys():
-            for e in extensions:
-                filename = filestem + e
-                if not os.path.exists(glacier + '/' + filename):
-                    url = glacier_data[glacier][filestem] + filename
-                    os.system('wget ' + url + ' -P ' + glacier)
+    website = "http://students.washington.edu/shapero/"
+    for glacier in glaciers:
+        for e in extensions:
+            filename = "mosaicOffsets" + e
+            if not os.path.exists(glacier + '/' + filename):
+                url = website + glacier + '/' + filename
+                os.system("wget " + url + ' -P ' + glacier)
 
 
     # -------------------------------------
@@ -104,4 +88,3 @@ def main(argv):                                                                #
 if __name__ == "__main__":                                                     #
 # ---------------------------------------------------------------------------- #
     main(sys.argv[1:])
-
