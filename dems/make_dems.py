f485d862f1e448e9fe614456a3605bc951b92176
danshapero <shapero.daniel@gmail.com>
2015-02-15 14:49:25 -0800

    Writing CReSIS DEMs to our format works

--- dems/make_dems.py
+++ dems/make_dems.py
@@ -54,7 +54,7 @@ def main(argv):                                                                #
             imin, imax = max(0, min(I) - 2), min(nx - 2, max(I) + 2)
             jmin, jmax = max(0, min(J) - 2), min(nx - 2, max(J) + 2)
             del I, J
-            
+
             vx = vx[imin: imax+1, jmin: jmax+1]
             vy = vy[imin: imax+1, jmin: jmax+1]
 
@@ -69,7 +69,7 @@ def main(argv):                                                                #
 
             for fid in [fidu, fidv]:
                 fid.write('{0}\n{1}\n'.format(nx, ny))
-                
+
             for j in range(nx):
                 for i in range(ny):
                     fidu.write('{0} {1} {2}\n'.format(x[j], y[i], vx[i,j]))
@@ -88,61 +88,91 @@ def main(argv):                                                                #
     # Make surface / bed elevation data
     # ----------------------------------
 
-    if dem_source == "morlighem":
-        morlighem_data = Dataset("../data/MCdataset-2014-11-19.nc", "r")
-        x = morlighem_data.variables['x'][:]
-        y = morlighem_data.variables['y'][:]
+    rects = {"helheim": ((249775.0, 310825.0),
+                         (-2592825.0, -2513625.0)),
+             "kangerd": ((448375.0, 495025.0),
+                         (-2300325.0, -2251725.0)),
+             "jakobshavn": ((-209985.0, -135015.0),
+                            (-2314985.0, -2245025.0))}
 
-        xmin, xmax = np.min(x), np.max(x)
-        ymin, ymax = np.min(y), np.max(y)
+    for glacier in glaciers:
+        # If we're using the mass-conserving bed DEM, extract the right window
+        # for each glacier from the big netCDF file for all of Greenland
+        if dem_source == "morlighem":
+            morlighem_data = Dataset("../data/MCdataset-2014-11-19.nc", "r")
+            x = morlighem_data.variables['x'][:]
+            y = morlighem_data.variables['y'][:]
 
-        dx = np.abs(x[1] - x[0])
-        dy = np.abs(y[1] - y[0])
+            xmin, xmax = np.min(x), np.max(x)
+            ymin, ymax = np.min(y), np.max(y)
 
-        rects = {"helheim": ((249775.0, 310825.0),
-                             (-2592825.0, -2513625.0)),
-                 "kangerd": ((448375.0, 495025.0),
-                             (-2300325.0, -2251725.0)),
-                 "jakobshavn": ((-209985.0, -135015.0),
-                                (-2314985.0, -2245025.0))}
+            dx = np.abs(x[1] - x[0])
+            dy = np.abs(y[1] - y[0])
 
-        for glacier, r in rects.iteritems():
+            r = rects[glacier]
+
             jmin = int( (r[0][0] - xmin) / dx )
             jmax = int( (r[0][1] - xmin) / dx )
             imin = int( (r[1][0] - ymin) / dy )
             imax = int( (r[1][1] - ymin) / dy )
 
-            nxp = jmax - jmin
-            nyp = imax - imin
+            nx = jmax - jmin
+            ny = imax - imin
 
-            xp = x[jmin:jmax]
-            yp = y[imin:imax]
+            x = x[jmin:jmax]
+            y = y[imin:imax]
 
-            B = np.zeros((nyp, nxp), dtype = np.float64)
-            S = np.zeros((nyp, nxp), dtype = np.float64)
+            B = np.zeros((ny, nx), dtype = np.float64)
+            S = np.zeros((ny, nx), dtype = np.float64)
             B[:,:] = morlighem_data.variables["bed"][imin:imax, jmin:jmax]
             S[:,:] = morlighem_data.variables["surface"][imin:imax, jmin:jmax]
 
-            yp = yp[::-1]
+            y = y[::-1]
             B = B[::-1, :]
             S = S[::-1, :]
 
-            fields = {"zbDEM.xy": B, "zsDEM.xy": S}
-            for filename, field in fields.iteritems():
-                file = open(glacier + '/' + filename, 'w')
-                file.write("{0}\n{1}\n".format(nxp, nyp))
-                for j in range(nxp):
-                    for i in range(nyp):
-                        if field[i, j] == -9999.0:
-                            field[i, j] = -2.0e+9
-                        file.write("{0} {1} {2}\n"
-                                   .format(xp[j], yp[i], field[i, j]))
+            morlighem_data.close()
 
-        morlighem_data.close()
+        elif dem_source == "cresis":
+            filename = "../data/" + glacier + "/" + glacier
+            cresis_bed_data = open(filename + "_composite_bottom.txt", "r")
+            cresis_surf_data = open(filename + "_composite_surface.txt", "r")
 
-    #else:
-        # TODO: unzip the files from CReSIS and convert their contents from
-        # the QGIS format to the one Elmer expects
+            nx = int(cresis_bed_data.readline().split()[1])
+            ny = int(cresis_bed_data.readline().split()[1])
+            xll = float(cresis_bed_data.readline().split()[1])
+            yll = float(cresis_bed_data.readline().split()[1])
+            dx = float(cresis_bed_data.readline().split()[1])
+            dy = dx
+            missing = float(cresis_bed_data.readline().split()[1])
+
+            for k in range(6):
+                cresis_surf_data.readline()
+
+            x = [xll + k * dx for k in range(nx)]
+            y = [yll + k * dy for k in range(ny)]
+
+            B = np.zeros((ny, nx), dtype = np.float64)
+            S = np.zeros((ny, nx), dtype = np.float64)
+
+            for k in range(ny):
+                B[k,:] = map(float, cresis_bed_data.readline().split())
+                S[k,:] = map(float, cresis_surf_data.readline().split())
+
+            cresis_bed_data.close()
+            cresis_surf_data.close()
+
+        # Write out the surface/bed DEMs
+        fields = {"zbDEM.xy": B, "zsDEM.xy": S}
+        for filename, field in fields.iteritems():
+            file = open(glacier + '/' + filename, 'w')
+            file.write("{0}\n{1}\n".format(nx, ny))
+            for j in range(nx):
+                for i in range(ny):
+                    if field[i, j] == -9999.0:
+                        field[i, j] = -2.0e+9
+                    file.write("{0} {1} {2}\n"
+                               .format(x[j], y[i], field[i, j]))
 
 
     if not os.path.exists("jakobshavn/zsDEM.xy"):
@@ -163,7 +193,7 @@ def main(argv):                                                                #
         else:
             os.system("wget " + surface_dem_url
                         + "jakobshavn/zsDEM.xy -P jakobshavn")
-       
+
 
     print ("Done making surface elevation for Jakobshavn")
 
@@ -176,4 +206,3 @@ def main(argv):                                                                #
 
 if __name__ == "__main__":
     main(sys.argv[1:])
-
