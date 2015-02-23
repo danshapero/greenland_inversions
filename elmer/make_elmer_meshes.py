2129d1970d981c02f86c8ad383263614a74d19b3
danshapero <shapero.daniel@gmail.com>
2015-02-23 11:37:30 -0800

    Correction for wider interpolation when extruding mesh with CReSIS DEM

--- elmer/make_elmer_meshes.py
+++ elmer/make_elmer_meshes.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+import sys
+import argparse
 import os
 
 glaciers = ["helheim", "kangerd", "jakobshavn"]
@@ -8,6 +10,20 @@ glaciers = ["helheim", "kangerd", "jakobshavn"]
 # ---------------------------------------------------------------------------- #
 def main(argv):                                                                #
 # ---------------------------------------------------------------------------- #
+    # Parse command line arguments
+    dem_source = "morlighem"
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-d", "--dem", required = True,
+                        help = "DEM source, either 'cresis' or 'morlighem'")
+    args, _ = parser.parse_known_args(argv)
+    dem_source = args.dem
+
+    if not dem_source in ("morlighem", "cresis"):
+        print("Unrecognized DEM source " + dem_source + ", should be\n"
+              "either \"cresis\" or \"morlighem\"\n")
+        sys.exit(1)
+
     for glacier in glaciers:
         # Make the directories for each glacier
         if not os.path.exists(glacier):
@@ -20,7 +36,7 @@ def main(argv):                                                                #
 
         # Make the bed.xyz and surf.xyz files because ExtrudeMesh is
         # hard-coded to use them.
-        if (not os.path.exists("../dems/" + glacier + "/bed.xyz") or 
+        if (not os.path.exists("../dems/" + glacier + "/bed.xyz") or
             not os.path.exists("../dems/" + glacier + "/surf.xyz")):
             os.system("sed '1,2d' ../dems/" + glacier
                         + "/zbDEM.xy > ../dems/" + glacier + "/bed.xyz")
@@ -31,10 +47,16 @@ def main(argv):                                                                #
         if not os.path.exists(glacier + '3d'):
             os.makedirs(glacier + '3d')
 
-        # Extrude the mesh
+        # Extrude the mesh. Need a different interpolation radius depending
+        # on whose DEM we're using.
+        dx = 250.0
+        if dem_source == "cresis":
+            dx = 750.0
+
         if not os.path.exists(glacier + "3d/mesh.header"):
-            os.system("../ExtrudeMesh " + glacier + ' ' + glacier + 
-                "3d 11 1 1 0 0 0 0 ../dems/" + glacier + " 250.0 3 -2.0e9")
+            os.system("../ExtrudeMesh " + glacier + " " + glacier
+                      + "3d 11 1 1 0 0 0 0 ../dems/" + glacier + " "
+                      + str(dx) + " 3 -2.0e9")
 
         if not os.path.exists(glacier + "3d/partitioning.4/part.1.header"):
             os.system("ElmerGrid 2 2 " + glacier + "3d -metis 4 -removeunused")
@@ -44,4 +66,3 @@ def main(argv):                                                                #
 if __name__ == "__main__":                                                     #
 # ---------------------------------------------------------------------------- #
     main(sys.argv[1:])
-
