76fa0bd2ed163395640d96e3bfb3f56eee4c0a7a
danshapero <shapero.daniel@gmail.com>
2015-04-07 15:39:28 -0700

    Wrote script for L-curve method
    Adds a script to run the model several times with successively larger
    values of the regularization parameter in order to probe which value is
    best by finding the inflection point the error/lambda curve

---
+++ L_curve.py
@@ -0,0 +1,36 @@
+
+from scripts.elmer import get_error_from_elmer_log
+import run
+
+import sys
+import numpy as np
+
+
+# ------------
+def main(argv):
+
+    rmin = float(argv[0])
+    rmax = float(argv[1])
+    nr   = int(argv[2])
+
+    rs = np.linspace(rmin, rmax, nr)
+    err = np.zeros(nr)
+
+    for n in range(nr):
+        r = rs[n]
+        log_file_name = "helheim_lambda-" + str(r) + ".txt"
+
+        run.main(["-g", "helheim",
+                  "-r", str(r),
+                  "-i", "20",
+                  "-o", log_file_name])
+
+        with open(log_file_name, "r") as log_file:
+            err[n] = get_error_from_elmer_log(log_file.read())
+
+    with open("L_curve_data.txt", "w") as output_file:
+        output_file.write("{0} {1}\n".format(rs[n], err[n]))
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
