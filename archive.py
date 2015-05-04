212119a630560358a64643f5fcf20a278bc4a244
danshapero <shapero.daniel@gmail.com>
2015-05-04 10:27:20 -0700

    archive.py packs any extra files passed to command line

--- archive.py
+++ archive.py
@@ -12,11 +12,15 @@ def main(argv):
                         help = "Name of glacier for inversion.")
     parser.add_argument("-o", "--output", required = True,
                         help = "Name of output file.")
+    parser.add_argument("-x", "--extra", required = False, nargs = '*',
+                        help = "Extra files to pack into the archive,"
+                               " e.g. log files.")
 
     args, _ = parser.parse_known_args(argv)
 
     glacier = args.glacier
     output  = args.output
+    extras  = args.extra
 
     tar = tarfile.open(name = output, mode = 'w:gz')
 
@@ -24,6 +28,9 @@ def main(argv):
         tar.add(dir + "/" + glacier)
 
     tar.add("elmer/" + glacier + "3d")
+
+    for filename in extras:
+        tar.add(filename)
 
     tar.close()
 
